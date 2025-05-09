f6823773c17022406f44bf75ea34430401de548f
Ana Tobon <anita.tobon28@gmail.com>
2025-05-09 18:46:20 -0500

    me rindo adios no quiero mas programar no es lo mio

--- template-parcial-2-main/src/index.ts
+++ template-parcial-2-main/src/index.ts
@@ -1,3 +1,14 @@
 import Root from "./Root/Root";
+import Card from "./Components/Card"
+import CardDescription from "./Components/CardDescrption"
+import Inicio from "./Components/inicio"
+import Modificar from "./Components/Modificar"
+import PlantModification from "./Components/PlantModification"
 
 customElements.define('root-element', Root);
+customElements.define("Card-Description", CardDescription);
+customElements.define("Inicio", Inicio);
+customElements.define("Modificar", Modificar);
+customElements.define("Plant-Modification", PlantModification);
+customElements.define("Card", Card);
+
